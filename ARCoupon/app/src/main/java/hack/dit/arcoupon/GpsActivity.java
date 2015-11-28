17ba2fbbfd7dc272207af178fdbc3cb6d5a680c7
chikam <c.coco.102@gmail.com>
2015-11-29 01:54:55 +0900

    getリクエストができるかどうかの確認

--- ARCoupon/app/src/main/java/hack/dit/arcoupon/GpsActivity.java
+++ ARCoupon/app/src/main/java/hack/dit/arcoupon/GpsActivity.java
@@ -7,7 +7,18 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class GpsActivity extends AppCompatActivity implements LocationListener {
 
@@ -33,8 +44,8 @@ public class GpsActivity extends AppCompatActivity implements LocationListener {
         String provider = mLocationManager.getBestProvider(criteria, true);
 
         // 取得したロケーションプロバイダを表示
-        TextView tv_provider = (TextView) findViewById(R.id.Provider);
-        tv_provider.setText("Provider: " + provider);
+        //TextView tv_provider = (TextView) findViewById(R.id.Provider);
+        //tv_provider.setText("Provider: " + provider);
 
         // LocationListenerを登録
         mLocationManager.requestLocationUpdates(provider, 0, 0, this);
@@ -43,13 +54,34 @@ public class GpsActivity extends AppCompatActivity implements LocationListener {
     @Override
     public void onLocationChanged(Location location) {
         // 緯度の表示
-        TextView tv_lat = (TextView) findViewById(R.id.Latitude);
-        tv_lat.setText("Latitude:" + location.getLatitude());
+        //TextView tv_lat = (TextView) findViewById(R.id.Latitude);
+        //tv_lat.setText("Latitude:" + location.getLatitude());
 
         // 経度の表示
-        TextView tv_lng = (TextView) findViewById(R.id.Longitude);
-        tv_lng.setText("Latitude:" + location.getLongitude());
+        //TextView tv_lng = (TextView) findViewById(R.id.Longitude);
+        //tv_lng.setText("Latitude:" + location.getLongitude());
 
+        String result = null;
+
+        Request request = new Request.Builder()
+                .url("http://weather.livedoor.com/forecast/webservice/json/v1?city=400040")
+                .get()
+                .build();
+
+        // クライアントオブジェクトを作って
+        OkHttpClient client = new OkHttpClient();
+
+        // リクエストして結果を受け取って
+        try {
+            Response response = client.newCall(request).execute();
+            result = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("result", result);
+        TextView tv_provider = (TextView) findViewById(R.id.Result);
+        tv_provider.setText(result);
     }
 
     @Override
