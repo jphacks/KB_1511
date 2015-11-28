7c9349cbc4daeb74e99225c1067fb49222122f11
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2015-11-29 05:51:30 +0900

    ファイル名の誤りの訂正

--- ARCoupon/app/src/main/java/hack/dit/arcoupon/ARCouponExtensionReceiver.java
+++ ARCoupon/app/src/main/java/hack/dit/arcoupon/ARCouponExtensionReceiver.java
@@ -9,7 +9,7 @@ import android.util.Log;
 /**
  * Created by kiyomaru on 15/11/29.
  */
-public class HelloWorldExtensionReceiver extends BroadcastReceiver {
+public class ARCouponExtensionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
         Log.d(Constants.LOG_TAG, "onReceive: " + intent.getAction());
