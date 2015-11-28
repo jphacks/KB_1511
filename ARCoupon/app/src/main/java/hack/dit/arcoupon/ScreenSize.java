b8972e35c225f86fec0724be48ad6728bd1c8eda
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2015-11-29 02:59:45 +0900

    ScreenSizeの追加

---
+++ ARCoupon/app/src/main/java/hack/dit/arcoupon/ScreenSize.java
@@ -0,0 +1,75 @@
+package hack.dit.arcoupon;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+/**
+ * Provides screen size.
+ *
+ * <p>This class is immutable.</p>
+ */
+public final class ScreenSize {
+
+    /** The screen width. */
+    private final int width;
+
+    /** The screen height. */
+    private final int height;
+
+    /** An application context. */
+    private final Context context;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param context An application context.
+     */
+    public ScreenSize(final Context context) {
+        this.context = context;
+        Resources res = context.getResources();
+        width = res.getDimensionPixelSize(R.dimen.smarteyeglass_control_width);
+        height = res.getDimensionPixelSize(
+                R.dimen.smarteyeglass_control_height);
+    }
+
+    /**
+     * Returns an application context.
+     *
+     * @return An application context.
+     */
+    public Context getContext() {
+        return context;
+    }
+
+    /**
+     * Returns the screen width.
+     *
+     * @return The screen width.
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Returns the screen height.
+     *
+     * @return The screen height.
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Compares this size to the specified size.
+     *
+     * @param width
+     *            The width of size.
+     * @param height
+     *            The height of size.
+     * @return {@code true} if the specified size is equal to this,
+     *         {@code false} otherwise.
+     */
+    public boolean equals(final int width, final int height) {
+        return this.width == width && this.height == height;
+    }
+}
