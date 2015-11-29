8d7b27af190dfb252ac0e9fd0a4ff5082852b174
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2015-11-29 09:25:30 +0900

    店舗・クーポン情報からマップ詳細への遷移を実装

--- ARCoupon/app/src/main/java/hack/dit/arcoupon/ARCouponActivity.java
+++ ARCoupon/app/src/main/java/hack/dit/arcoupon/ARCouponActivity.java
@@ -6,59 +6,51 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by kiyomaru on 15/11/29.
  */
-public class ARCouponActivity extends Activity {
+public class ARCouponActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
     @Override
-    public void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
-        // When button is clicked, run the SmartEyeglass app
-        //Button btnGlass = (Button) findViewById(R.id.btnglass);
-        /*
-        btnGlass.setOnClickListener(new OnClickListener() {
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-            @Override
-            public void onClick(final View v) {
-                startExtension();
-            }
-        });
-        */
-
-        startExtension();
-
-        /*
-         * Check if activity was started with a message in the intent
-         * If there is a message, show it as a Toast message
-         */
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String message = extras.getString("Message");
-            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG)
-                    .show();
-        }
-
-        /*
-         * Make sure ExtensionService of your SmartEyeglass app has already
-         * started.
-         * This is normally started automatically when user enters your app
-         * on SmartEyeglass, although you can initialize it early using
-         * request intent.
-         */
-        if (ARCouponExtensionService.Object == null) {
-            Intent intent = new Intent(Registration.Intents
-                    .EXTENSION_REGISTER_REQUEST_INTENT);
-            Context context = getApplicationContext();
-            intent.setClass(context, ARCouponExtensionService.class);
-            context.startService(intent);
-        }
+        // 緯度経度の登録
+        LatLng location = new LatLng(34.694524, 135.195996);
+        // カメラポジション設定
+        CameraPosition cameraPos = new CameraPosition.Builder().target(location).zoom(18.0f).bearing(0).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPos));
+        // マーカーを作成
+        Marker marker = mMap.addMarker(new MarkerOptions().position(location).title("お店の名前").snippet("クーポンの内容"));
+        // インフォウィンドウ表示
+        marker.showInfoWindow();
     }
 
     /**
